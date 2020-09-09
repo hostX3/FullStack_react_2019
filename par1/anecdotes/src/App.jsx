8e61c3238dd973eab873ebdfbcb456802507e53a
Carlos Garcia <carlosmauriciogarcia13@gmail.com>
2020-09-09 05:07:23 -0400

    Completed 1.14*: anecdotes step3

--- par1/anecdotes/src/App.jsx
+++ par1/anecdotes/src/App.jsx
@@ -5,6 +5,7 @@ const App = ({ anecdotesList }) => {
   const [anecdotes, setAnecdotes] = useState([...anecdotesList])
   const [selected, setSelected] = useState(0)
 
+  
   const randomAnecdoteHandler = (anecdotesNum) => {
     const randomAnecdoteNumber = Math.floor(Math.random() * anecdotesNum)
     setSelected(randomAnecdoteNumber)
@@ -24,14 +25,32 @@ const App = ({ anecdotesList }) => {
     })
     setAnecdotes(votedAnecdotes);
   }
- 
+
+  const mostVotdAnecdoteHandler = (anecdotes) => {
+    let mostVoted = {
+      anecdote: 'No hay anecdotas votadas aun',
+      voteCount: 0
+    }
+    anecdotes.map((item, index) => {
+      if (item.voteCount > mostVoted.voteCount){
+        mostVoted = item
+      }
+    })
+    return mostVoted.anecdote
+  }
+
 
   return (
     <div>
+      <p>Anecdote of the day</p>
       <p>{anecdotes[selected].anecdote}</p>
       <p>{anecdotes[selected].voteCount}</p>
       <button type={'buton'} onClick={() => randomAnecdoteHandler(anecdotes.length)} > Random Anecdote </button>
-      <button type={'button'} onClick={()=> voteHandler(selected)} >vote</button>
+      <button type={'button'} onClick={() => voteHandler(selected)} >vote</button>
+
+      <p>Most voted anecdote</p>
+      <p>{mostVotdAnecdoteHandler(anecdotes)}</p>
+
     </div>
   )
 }
