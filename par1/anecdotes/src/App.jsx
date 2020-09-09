ef0f87fed3b46ffb5743ef534cdd70a181758fd7
Carlos Garcia <carlosmauriciogarcia13@gmail.com>
2020-09-09 04:52:25 -0400

    Completed 1.13*: anecdotes step2

--- par1/anecdotes/src/App.jsx
+++ par1/anecdotes/src/App.jsx
@@ -1,19 +1,37 @@
 import React, { useState } from 'react'
 
 
-const App = ({anecdotes}) => {
+const App = ({ anecdotesList }) => {
+  const [anecdotes, setAnecdotes] = useState([...anecdotesList])
   const [selected, setSelected] = useState(0)
 
-  const randomAnecdoteHandler = (anecdotesNum)=>{
+  const randomAnecdoteHandler = (anecdotesNum) => {
     const randomAnecdoteNumber = Math.floor(Math.random() * anecdotesNum)
     setSelected(randomAnecdoteNumber)
   }
 
+  const voteHandler = (currentAnecdote) => {
+
+    const votedAnecdotes = anecdotes.map((item, index) => {
+      if (index === currentAnecdote) {
+        return {
+          ...item,
+          voteCount: item.voteCount + 1
+        }
+      } else {
+        return item
+      }
+    })
+    setAnecdotes(votedAnecdotes);
+  }
+ 
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <button type={'buton'} onClick={()=>randomAnecdoteHandler(anecdotes.length)} > Random Anecdote </button>
+      <p>{anecdotes[selected].anecdote}</p>
+      <p>{anecdotes[selected].voteCount}</p>
+      <button type={'buton'} onClick={() => randomAnecdoteHandler(anecdotes.length)} > Random Anecdote </button>
+      <button type={'button'} onClick={()=> voteHandler(selected)} >vote</button>
     </div>
   )
 }
